bdc3a6b20f4445056b5e32e1902b96e953e795a3
Jain <anmol.jain@aciworldwide.com>
2020-02-22 16:49:37 +0530

    CRUD operations added

--- src/main/java/com/aciworldwide/acisportsstoreapi/controller/ProductController.java
+++ src/main/java/com/aciworldwide/acisportsstoreapi/controller/ProductController.java
@@ -4,7 +4,12 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -23,6 +28,26 @@ public class ProductController {
 		return productService.getProducts();
 	}
 	
+	@DeleteMapping("/delete")
+	public void deleteProduct(@RequestBody Long id) {
+		
+		productService.deleteProduct(id);
+	}
 	
+	@PutMapping("/update")
+	public Product updateProduct(@RequestBody Product p) {
+		
+		return productService.updateProduct(p);
+	}
+	
+	@PostMapping("/insert")
+	public Product insertProduct(@RequestBody Product p) {
+		return productService.insertProduct(p);
+	}
+	
+	@GetMapping("/{id}")
+	public Product getProduct(@PathVariable long id) {
+		return productService.getProduct(id);
+	}
 	
 }
--- src/main/java/com/aciworldwide/acisportsstoreapi/services/ProductService.java
+++ src/main/java/com/aciworldwide/acisportsstoreapi/services/ProductService.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.PathVariable;
 
 import com.aciworldwide.acisportsstoreapi.model.Product;
 import com.aciworldwide.acisportsstoreapi.repository.ProductRepository;
@@ -12,14 +13,30 @@ import com.aciworldwide.acisportsstoreapi.repository.ProductRepository;
 public class ProductService {
 	@Autowired
 	private ProductRepository repository;
-	
-	public List<Product> getProducts(){
+
+	public List<Product> getProducts() {
 		return repository.findAll();
 	}
-	
-	public Product getProduct(Long id) {
-		return repository.getOne(id);
+
+	public Product getProduct(long id) {
+		return repository.findById(id).get();
+	}
+
+	public Product putProduct(Product p) {
+		return repository.saveAndFlush(p);
+	}
+
+	public Product updateProduct(Product p) {
+		
+		return repository.saveAndFlush(p);
+	}
+
+	public void deleteProduct(long id) {
+		repository.deleteById(id);
 	}
 	
-	
+	public Product insertProduct(Product p) {
+		return repository.save(p);
+	}
+
 }
